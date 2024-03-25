87313dfd1daa7f7a201a4127f68f0d09a79c2478
dvvteja <dvvteja@gmail.com>
2024-03-25 13:15:14 +0530

    pagination added

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # Employee Management System
 Employee Management System using Spring Boot, Thymeleaf and MySQL database.
-
-# YouTube Video Series By RameshMF (Java Guides)
-## Employee Management Module
-<ul>
-<li><a href="https://youtu.be/U7WDfoiodOg" target="_blank">Spring Boot Thymeleaf CRUD Database Real-Time Project - PART 1 </a></li>
-<li><a href="https://youtu.be/bcV0WtOoY54" target="_blank">Spring Boot Thymeleaf CRUD Database Real-Time Project - PART 2 </a></li>
-<li><a href="https://youtu.be/dlGmqzex8GE" target="_blank">Spring Boot Thymeleaf CRUD Database Real-Time Project - PART 3 </a></li>
-<li><a href="https://youtu.be/iVAZ1bK8YGI" target="_blank">Spring Boot Thymeleaf CRUD Database Real-Time Project - PART 4 </a></li>
-<li><a href="https://youtu.be/gUapbepRa24" target="_blank">Spring Boot Thymeleaf CRUD Database Real-Time Project - PART 5 </a></li>
-</ul>
--- src/main/java/com/tejadvv/springbootthymeleafcrudwebapp/service/EmployeeServiceImpl.java
+++ src/main/java/com/tejadvv/springbootthymeleafcrudwebapp/service/EmployeeServiceImpl.java
@@ -2,6 +2,10 @@ package com.tejadvv.springbootthymeleafcrudwebapp.service;
 
 import com.tejadvv.springbootthymeleafcrudwebapp.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import com.tejadvv.springbootthymeleafcrudwebapp.repository.EmployeeRepository;
 
@@ -39,4 +43,14 @@ public class EmployeeServiceImpl implements EmployeeService{
     public void deleteEmployeeById(long id) {
         this.employeeRepository.deleteById(id);
     }
+
+    @Override
+    public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+        return this.employeeRepository.findAll(pageable);
+    }
+
 }
